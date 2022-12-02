5630b71d6fcb1be13ecd6fcaffd894e5357c002b
420factorauthentication <420factorauthentication@gmail.com>
2022-12-02 01:43:17 -0800

    fix main menu responsive design

--- src/dlcd/src/scene/title.ts
+++ src/dlcd/src/scene/title.ts
@@ -4,8 +4,14 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 const TitleScene: {
-    /** Static main menu art. */
+    /** SVG title text. */
+    title?: HTMLElement;
+
+    /** SVG art. */
     art?: HTMLElement;
+
+    /** Animated space background. */
+    bg?: HTMLElement;
 
     /** Is this Scene currently displaying it's DOM Elements? */
     isActive?: boolean;
@@ -18,21 +24,57 @@ const TitleScene: {
 } = {
     /** Creates this Scene's DOM Elements. */
     activate() {
+        // Do nothing if active
         if (TitleScene.isActive) return;
         TitleScene.isActive = true;
 
-        TitleScene.art = <HTMLElement>document.createElement("img");
+        // Title create
+        TitleScene.title = <HTMLElement>document.createElement("div");
+        document.body.appendChild(TitleScene.title);
+
+        // Title top-middle with margins
+        TitleScene.title.style.position = "absolute";
+        TitleScene.title.style.left = "5vw";
+        TitleScene.title.style.top = "5vh";
+        TitleScene.title.style.width = "90vw";
+        TitleScene.title.style.height = "calc(90vh * 280 / 900)";
+        TitleScene.title.style.zIndex = "2";
+
+        // Title svg
+        TitleScene.title.style.backgroundImage =
+            "url(./assets/art/mm-title.svg)";
+        TitleScene.title.style.backgroundSize = "100% 100%";
+        TitleScene.title.style.backgroundRepeat = "no-repeat";
+
+        // Art create
+        TitleScene.art = <HTMLElement>document.createElement("div");
         document.body.appendChild(TitleScene.art);
+
+        // Art bottom-left with margins
         TitleScene.art.style.position = "absolute";
         TitleScene.art.style.left = "5vw";
-        TitleScene.art.style.top = "5vh";
-        TitleScene.art.style.width = "90vw";
-        TitleScene.art.style.height = "90vh";
-        TitleScene.art.style.backgroundImage =
-            "url(./assets/art/MainMenuArt.svg)";
-        TitleScene.art.style.backgroundColor = "#515151";
+        TitleScene.art.style.bottom = "5vh";
+        TitleScene.art.style.width = "calc(90vw * 780 / 1440)";
+        TitleScene.art.style.height = "calc(90vh * 620 / 900)";
+        TitleScene.art.style.zIndex = "2";
+
+        // Art svg
+        TitleScene.art.style.backgroundImage = "url(./assets/art/mm-art0.svg)";
         TitleScene.art.style.backgroundSize = "100% 100%";
         TitleScene.art.style.backgroundRepeat = "no-repeat";
+
+        // BG create
+        TitleScene.bg = <HTMLElement>document.createElement("div");
+        document.body.appendChild(TitleScene.bg);
+        TitleScene.bg.style.backgroundColor = "#515151";
+
+        // BG centered
+        TitleScene.bg.style.position = "absolute";
+        TitleScene.bg.style.left = "0";
+        TitleScene.bg.style.top = "0";
+        TitleScene.bg.style.width = "100vw";
+        TitleScene.bg.style.height = "100vh";
+        TitleScene.bg.style.zIndex = "1";
     },
 
     /** Destroy this Scene's DOM Elements and cleanup all garbage. */
@@ -40,8 +82,11 @@ const TitleScene: {
         if (!TitleScene.isActive) return;
         TitleScene.isActive = false;
 
-        TitleScene.art.remove();
-        delete TitleScene.art;
+        TitleScene.title.remove();
+        delete TitleScene.title;
+
+        TitleScene.bg.remove();
+        delete TitleScene.bg;
     },
 };
 
