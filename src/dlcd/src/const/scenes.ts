201905e267badf5c699fa0e39d3957bd4d1eb3ca
420factorauthentication <420factorauthentication@gmail.com>
2023-07-12 18:01:21 -0700

    Added Less CSS to workflow

--- src/dlcd/src/const/scenes.ts
+++ src/dlcd/src/const/scenes.ts
@@ -7,7 +7,7 @@ import SceneLink from "../../../lib-scene/slink.js";
 ////////////////////////////////////////////////////////////////////////////////
 
 export const GameStyles: Readonly<SceneLink> = new SceneLink(
-    "src/style/game.css"
+    "../../_out/dlcd/src/style/game.css"
 );
 
 export const StartScreen: Readonly<Scene> = new Scene("src/scene/start.html");
---
+++ utils/buildLess.py
@@ -0,0 +1,19 @@
+import os
+
+#########################################
+# Builds all .less files in src to _out #
+#########################################
+
+
+inDir = "src"
+outDir = "_out"
+
+
+for subdir, dirs, files in os.walk(inDir):
+    for filename in files:
+        if filename.endswith(".less"):
+            inPath = subdir + os.sep + filename
+            childDir = subdir.split(os.sep, 1)[1]
+            newfilename = filename.split(".less")[0] + ".css"
+            outPath = outDir + os.sep + childDir + os.sep + newfilename
+            os.system("lessc " + inPath + " " + outPath)
