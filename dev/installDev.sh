6c9a2fd85b50e5185b64d69109b553c5e07c277b
420factorauthentication <420factorauthentication@gmail.com>
2023-07-26 22:14:28 -0700

    update dev/installDev.sh

--- dev/installDev.sh
+++ dev/installDev.sh
@@ -10,4 +10,11 @@ sudo npm install -g less
 echo "\n\n"
 echo "==== INSTALL VSCODE EXTENSIONS ===="
 code --install-extension esbenp.prettier-vscode
+code --install-extension ritwickdey.LiveServer
 code --install-extension zfzackfrost.commentbars
+code --install-extension ZainChen.json
+code --install-extension skellock.just
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.isort
+code --install-extension ms-python.black-formatter
